f6d6bf78733018d096ea7d467c3abeabf6c83d3f
Felipe Melchior <fehmel@gmail.com>
2024-08-15 23:15:13 -0300

    add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Isso é apenas um teste de deploy</h1>
+  return <h1>Isso é apenas um teste de deploy</h1>;
+}
+
+function test() {
+  console.log("test");
+}
+
+function test2() {
+  console.log("test");
 }
 
 export default Home;

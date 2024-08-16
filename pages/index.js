0576a49535fb393a3b4c8314642d7da87b8197c6
Felipe Melchior <fehmel@gmail.com>
2024-08-16 13:17:17 -0300

    add jest test runner

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Isso é apenas um teste de deploy</h1>;
 }
 
-function test() {
-  console.log("test");
-}
-
-function test2() {
-  console.log("test");
-}
-
 export default Home;

427877e9b1545b25e9f1431736df89c583b1577a
Felipe Melchior <fehmel@gmail.com>
2024-08-17 17:22:44 -0300

    add `database.js`

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+const config = {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+async function query(queryObject) {
+  const client = new Client(config);
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select current_user;");
+  console.log(result.rows);
   response.status(200).json({ status: "Healthy" });
 }
 

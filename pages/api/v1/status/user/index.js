01c995b062fda6e95b851941afda035618340dad
Felipe Melchior <fehmel@gmail.com>
2024-08-18 00:17:48 -0300

    change endpoint to `/api/v1/status/user`

--- pages/api/v1/status/user/index.js
+++ pages/api/v1/status/user/index.js
@@ -1,8 +1,8 @@
 import database from "infra/database";
 
-async function databaseStatus(request, response) {
+async function currentUser(request, response) {
   const result = await database.query("select current_user;");
   response.status(200).json(result.rows[0]);
 }
 
-export default databaseStatus;
+export default currentUser;
--- tests/integration/api/v1/status/database/get.test.js
+++ tests/integration/api/v1/status/database/get.test.js
@@ -1,14 +1,14 @@
 async function fetchData() {
-  const response = await fetch("http://localhost:3000/api/v1/status/database");
+  const response = await fetch("http://localhost:3000/api/v1/status/user");
   return response;
 }
 
-test("GET /api/v1/status/database should return status 200", async () => {
+test("GET /api/v1/status/user should return status 200", async () => {
   const response = await fetchData();
   expect(response.status).toBe(200);
 });
 
-test("GET /api/v1/status/database should return postgres user", async () => {
+test("GET /api/v1/status/user should return postgres user", async () => {
   const response = await fetchData();
   expect(response.json()).resolves.toEqual({ current_user: "postgres" });
 });

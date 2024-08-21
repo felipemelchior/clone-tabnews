f4023dd68a5064253be12e37e1d5cd2ef2b159ce
Felipe Melchior <fehmel@gmail.com>
2024-08-20 21:39:36 -0300

    improve db client connection management

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,9 @@
 import { Client } from "pg";
 
-const config = {
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: getSSLValues(),
-};
-
 async function query(queryObject) {
-  const client = new Client(config);
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
@@ -32,6 +23,21 @@ function getSSLValues() {
   return process.env.NODE_ENV === "production" ? true : false;
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
